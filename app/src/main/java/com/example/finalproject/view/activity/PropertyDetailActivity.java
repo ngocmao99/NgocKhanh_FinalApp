072a5eaf6ccbc2539d15ac86f84f096153775b80
ngocmao99 <ngocmao99@gmail.com>
2022-12-08 18:17:43 +0700

    new branch

--- app/src/main/java/com/example/finalproject/view/activity/PropertyDetailActivity.java
+++ app/src/main/java/com/example/finalproject/view/activity/PropertyDetailActivity.java
@@ -1,8 +1,10 @@
-package com.example.finalproject.view.activity.onboardingfragment;
+package com.example.finalproject.view.activity;
+
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.finalproject.R;
 
 public class PropertyDetailActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/finalproject/view/adapter/PropertyAdapter.java
@@ -0,0 +1,41 @@
+package com.example.finalproject.view.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.finalproject.models.Property;
+
+import java.util.List;
+
+public class PropertyAdapter extends RecyclerView.Adapter<PropertyAdapter.ViewHolder>{
+
+    private Context mContext;
+    List<Property> properties;
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
